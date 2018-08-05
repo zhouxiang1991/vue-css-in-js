ade3e70da6c73d7d2bf38c7ce0c4d2b14c7e97cf
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2018-08-05 13:29:14 +0800

    class hash前加_ 防止第一个字符是数字

--- src/utils.js
+++ src/utils.js
@@ -1,69 +1,3 @@
-const preserveCamelCase = (input) => {
-  let isLastCharLower = false;
-  let isLastCharUpper = false;
-  let isLastLastCharUpper = false;
-
-  for (let i = 0; i < input.length; i += 1) {
-    const c = input[i];
-
-    if (isLastCharLower && /[a-zA-Z]/.test(c) && c.toUpperCase() === c) {
-      input = `${input.slice(0, i)}-${input.slice(i)}`;
-      isLastCharLower = false;
-      isLastLastCharUpper = isLastCharUpper;
-      isLastCharUpper = true;
-      i += 1;
-    } else if (isLastCharUpper && isLastLastCharUpper && /[a-zA-Z]/.test(c) && c.toLowerCase() === c) {
-      input = `${input.slice(0, i - 1)}-${input.slice(i - 1)}`;
-      isLastLastCharUpper = isLastCharUpper;
-      isLastCharUpper = false;
-      isLastCharLower = true;
-    } else {
-      isLastCharLower = c.toLowerCase() === c;
-      isLastLastCharUpper = isLastCharUpper;
-      isLastCharUpper = c.toUpperCase() === c;
-    }
-  }
-
-  return input;
-};
-
-export const camelCase = (input, options) => {
-  options = {
-    pascalCase: false,
-    ...options,
-  };
-
-  const postProcess = x => (options.pascalCase ? x.charAt(0).toUpperCase() + x.slice(1) : x);
-
-  if (Array.isArray(input)) {
-    input = input.map(x => x.trim()).filter(x => x.length).join('-');
-  } else {
-    input = input.trim();
-  }
-
-  if (input.length === 0) {
-    return '';
-  }
-
-  if (input.length === 1) {
-    return options.pascalCase ? input.toUpperCase() : input.toLowerCase();
-  }
-
-  if (/^[a-z\d]+$/.test(input)) {
-    return postProcess(input);
-  }
-
-  const hasUpperCase = input !== input.toLowerCase();
-
-  if (hasUpperCase) {
-    input = preserveCamelCase(input);
-  }
-
-  input = input.replace(/^[_.\- ]+/, '').toLowerCase().replace(/[_.\- ]+(\w|$)/g, (m, p1) => p1.toUpperCase());
-
-  return postProcess(input);
-};
-
 export const getCssStyles = () => {
   const styles = window.getComputedStyle(document.documentElement, '');
   return [].slice.call(styles);
@@ -91,7 +25,8 @@ const stringHash = (str) => {
 export const hash = (_string, _length = 5) => {
   const string = String(_string);
   const length = isNaN(_length) ? 5 : parseInt(_length, 10);
-  const hash = stringHash(string).toString(36).substr(0, length);
+  let hash = stringHash(string).toString(36).substr(0, length);
+  hash = `_${hash}`;
   return hash;
 };
 
